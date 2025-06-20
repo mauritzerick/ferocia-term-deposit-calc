1c6ef6a014f40cef15c0067dcc19160a2177118e
Mauritz Erick <mauritzerick@Mauritzs-MacBook-Air.local>
2025-06-20 15:58:51 +1000

    feat: import calculator service to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CalculatorService, Frequency } from './calculator.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'ferocia-term-deposit-calc';
+  form = {
+    amount: 10000,
+    rate: 1.1,
+    term: 3,
+    frequency: 'maturity' as Frequency
+  };
+  result: number | null = null;
+
+  constructor(private calcService: CalculatorService) {}
+
+  calculate() {
+    const { amount, rate, term, frequency } = this.form;
+    this.result = this.calcService.calculate(amount, rate, term, frequency);
+  }
 }
